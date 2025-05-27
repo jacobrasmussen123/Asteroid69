1a781d9bf390884438a109b3306821dfa1f94882
jacobrasmussen123 <144017516+jacobrasmussen123@users.noreply.github.com>
2025-05-27 14:08:15 +0200

    SplitPackages implemented

--- Player/src/main/java/dk/sdu/cbse/player/PlayerPlugin.java
+++ Player/src/main/java/dk/sdu/cbse/player/PlayerPlugin.java
@@ -1,16 +1,20 @@
-package dk.sdu.cbse.player.shared;
+package dk.sdu.cbse.player;
 
 import dk.sdu.cbse.common.data.GameData;
 import dk.sdu.cbse.common.data.World;
 import dk.sdu.cbse.common.services.IGamePluginService;
 import dk.sdu.cbse.player.Player;
 
-public class SharedPlugin implements IGamePluginService {
-
+public class PlayerPlugin implements IGamePluginService {
+    private String playerId;
 
     @Override
     public void start(GameData gameData, World world) {
-
+        Player player = new Player();
+        player.setX(gameData.getDisplayWidth()  / 2.0);
+        player.setY(gameData.getDisplayHeight() / 2.0);
+        playerId = world.addEntity(player);
+    }
 
     @Override
     public void stop(GameData gameData, World world) {
--- Player/src/main/java/module-info.java
+++ Player/src/main/java/module-info.java
@@ -4,10 +4,10 @@ module Player {
 
     exports dk.sdu.cbse.player;          // renamed from dk.sdu.cbse.player.main
     exports dk.sdu.cbse.player.systems;
-    exports dk.sdu.cbse.player.shared;       // renamed shared package
+  // renamed shared package
 
     provides dk.sdu.cbse.common.services.IGamePluginService
-            with dk.sdu.cbse.player.shared.SharedPlugin;
+            with dk.sdu.cbse.player.PlayerPlugin;
     provides dk.sdu.cbse.common.services.IEntityProcessingService
             with dk.sdu.cbse.player.systems.PlayerControlSystem;
     uses dk.sdu.cbse.common.bullet.BulletSPI;
